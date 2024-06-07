b30aa4125e2443db736f1459b06f1c37b1ecf114
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2024-06-07 19:54:18 +0530

    Form completed

---
+++ chai/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from .models import ChaiVariety
+
+class ChaiVarietyForm(forms.Form):
+    chai_varity = forms.ModelChoiceField(queryset=ChaiVariety.objects.all(), label="Select chai variety")
